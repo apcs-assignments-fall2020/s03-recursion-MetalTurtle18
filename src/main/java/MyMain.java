9a945defabc70bb5e5f48e2ad023c99c7e1b24eb
MetalTurtle <dekolis1218@gmail.com>
2021-02-02 08:56:18 -0800

    Create three recursive methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,23 +2,20 @@ public class MyMain {
     // This method adds up the values of all digits
     // in x, recursively
     public static int addDigits(int x) { 
-        // YOUR CODE HERE
-        return -1;
+        return x < 10 ? x : (x % 10 + addDigits(x / 10));
     }
 
 
     // This method checks if a String is a palindrome
     // (e.g. "racecar", "madam"), recursively
     public static boolean isPalindrome(String str) { 
-        // YOUR CODE HERE
-        return false;
+        return str.length() <= 1 || (str.charAt(0) == str.charAt(str.length() - 1) && isPalindrome(str.substring(1, str.length() - 1)));
     }
 
     // This method returns the orignal string reversed;
     // this method should be written using recursion
     public static String reverse(String str) { 
-        // YOUR CODE HERE
-        return "";
+        return str.length() == 1 ? str : (str.charAt(str.length() - 1) + reverse(str.substring(0, str.length() - 1)));
     }
 
     public static void main(String[] args) {
